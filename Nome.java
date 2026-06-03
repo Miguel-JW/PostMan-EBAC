483b094f279d9d95d2498b3dd28c0f85a335b298
Miguel-JW <conteudojurassicojw@gmail.com>
2026-06-03 09:21:38 -0300

    Create Nome.java

---
+++ Nome.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "nomes")
+public class Nome {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+}
